57ffa9d77a0baf089036b797f05825b8e9331da5
Brett Mitchell <bmitchell@my365-uwc.ac.za>
2020-12-07 15:12:05 +0200

    Add project files.

---
+++ LocalDatabaseTutorial/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LocalDatabaseTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetPeopleAsync();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(nameEntry.Text) && !string.IsNullOrWhiteSpace(sizeEntry.Text))
+            {
+                Person newPerson = new Person();
+                newPerson.Name = nameEntry.Text;
+                newPerson.Size = int.Parse(sizeEntry.Text);
+                newPerson.Gender = genderEntry.Text;
+                newPerson.Color = colorEntry.Text;
+                newPerson.Address = addressEntry.Text;
+                await App.Database.SavePersonAsync(newPerson);
+
+                
+                listView.ItemsSource = await App.Database.GetPeopleAsync();
+            }
+        }
+    }
+}
---
+++ LocalDatabaseTutorial/Person.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace LocalDatabaseTutorial
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string Gender { get; set; }
+        public string Color { get; set; }
+        public string Address { get; set; }
+    }
+}
